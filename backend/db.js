c63a8c52e51ee94d0ba013234c0cd1cbc84a0355
Emily Pui <millily11024@gmail.com>
2026-05-25 03:14:08 +0800

    Update website

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,27 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: "sarakway-database.cnywtutq8hur.us-east-1.rds.amazonaws.com",
   user: "admin",
   password: "sarakwayadmin",
   database: "SarakWay_Database",
-  port: 3306
+  port: 3306,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
+
   if (err) {
     console.error("❌ Error connecting to MySQL:", err);
     return;
   }
 
   console.log("✅ Connected to AWS RDS MySQL Database");
+
+  connection.release();
 });
 
-module.exports = db;
+module.exports = pool.promise();
