6494a41e130a90d1445d5b948ffcd4603ce3853b
tylsee <165485198+tylsee@users.noreply.github.com>
2026-05-25 02:52:50 +0800

    database

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,20 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: "sarakway-database.cnywtutq8hur.us-east-1.rds.amazonaws.com",
   user: "admin",
   password: "sarakwayadmin",
   database: "SarakWay_Database",
-  port: 3307
+  port: 3306
 });
 
-db.connect(err => {
+db.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL:', err);
+    console.error("❌ Error connecting to MySQL:", err);
     return;
   }
-  console.log('✅ Connected to SarakWay Database on port 3307');
+
+  console.log("✅ Connected to AWS RDS MySQL Database");
 });
 
 module.exports = db;
