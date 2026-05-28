8c3eb0f8a921e191bbd0ee469bc3b3ef904ad391
tylsee <165485198+tylsee@users.noreply.github.com>
2026-05-28 18:45:37 +0800

    Update main.jsx

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -4,6 +4,19 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+// 1. Import Amplify
+import { Amplify } from 'aws-amplify';
+
+// 2. Configure Cognito (Replace with your actual IDs)
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'us-east-1_L5j6Nvm4a',
+      userPoolClientId: '7b7krehjq1tdoo6h9qof35qn6l',
+    }
+  }
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
