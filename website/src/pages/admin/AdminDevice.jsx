69e9f3473857ec9187dd3e0633e69b7e304fb2e2
tylsee <165485198+tylsee@users.noreply.github.com>
2026-05-29 16:13:33 +0800

    Update AdminDevice.jsx

--- website/src/pages/admin/AdminDevice.jsx
+++ website/src/pages/admin/AdminDevice.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "../../styles/alerts.css";
 
 export default function AdminDevice() {
   const [recordings, setRecordings] = useState([]);
