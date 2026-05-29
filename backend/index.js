1ad0f46ca1c4b07127f48b997f750170db4af750
tylsee <165485198+tylsee@users.noreply.github.com>
2026-05-29 14:56:18 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,7 @@ app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json({ limit: "50mb" })); 
 
